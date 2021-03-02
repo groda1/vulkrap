f3236b5c602bf340267b15546a2de1e769bcdd83
Adam Andersson <geheaz@gmail.com>
2021-03-02 21:53:45 +0100

    testing large memory allocs

---
+++ src/util/mem.rs
@@ -0,0 +1,13 @@
+
+pub fn alloc() {
+
+  //  use std::alloc::{alloc, Layout};
+  //  let layout = Layout::new::<[u32; 2_000_000]>();
+   // let mut buf = unsafe {
+    //    let ptr = alloc(layout) as *mut [u32; 2_000_000];
+     //   Box::from_raw(ptr)
+    //};
+
+
+}
+
